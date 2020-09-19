9018676c2ed7b6b1c4f13734f740dbbc60217ebc
99002438 <71484503+99002438@users.noreply.github.com>
2020-09-19 11:12:54 +0530

    Update calculator_operations.h

--- inc/calculator_operations.h
+++ inc/calculator_operations.h
@@ -1,15 +1,13 @@
 #ifndef __CALCULATOR_OPERATIONS_H__
 #define __CALCULATOR_OPERATIONS_H__
 #include<stdio.h>
-#include<conio.h>
 #include<math.h>
 #include<stdlib.h>
-#include<stdio_ext.h>
-int add(int number1,int number2);
-int subtract(int number1,int number2);
-int multiply(int number1,int number2);
-int division(int number1,int number2);
-int modulus(int number1,int number2);
-int power(int number1,int number2);
-int factorial(int number1);
+int add(int,int);
+int subtract(int,int);
+int multiply(int,int);
+int division(int,int);
+int modulus(int,int);
+int power(int,int);
+int factorial(int);
 #endif
