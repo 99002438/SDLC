17c8d6584401031ec5e57748ce1d9c9d659f7f39
99002438 <71484503+99002438@users.noreply.github.com>
2020-09-19 09:02:26 +0530

    Add files via upload

---
+++ inc/calculator_operations.h
@@ -0,0 +1,15 @@
+#ifndef __CALCULATOR_OPERATIONS_H__
+#define __CALCULATOR_OPERATIONS_H__
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<stdio_ext.h>
+int add(int number1,int number2);
+int subtract(int number1,int number2);
+int multiply(int number1,int number2);
+int division(int number1,int number2);
+int modulus(int number1,int number2);
+int power(int number1,int number2);
+int factorial(int number1);
+#endif
